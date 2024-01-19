ac73465a140c58bab60925c9b2723b7e946a0c20
chinapalace <justinmclaren63@gmail.com>
2024-01-19 14:41:19 -0800

    WIP

--- components/grid/index.tsx
+++ components/grid/index.tsx
@@ -1,33 +1,8 @@
-import clsx from 'clsx';
-
-function Grid(props: React.ComponentProps<'ul'>) {
-  return (
-    <ul {...props} className={clsx('grid grid-flow-row gap-4', props.className)}>
-      {props.children}
-    </ul>
-  );
-}
-
-function GridItem(props: React.ComponentProps<'li'>) {
-  return (
-    <li {...props} className={clsx('aspect-square transition-opacity', props.className)}>
-      {props.children}
-    </li>
-  );
-}
-
-Grid.Item = GridItem;
-
-export default Grid;
-
 // import clsx from 'clsx';
 
 // function Grid(props: React.ComponentProps<'ul'>) {
 //   return (
-//     <ul
-//       {...props}
-//       className={clsx('grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3', props.className)}
-//     >
+//     <ul {...props} className={clsx('grid grid-flow-row gap-4', props.className)}>
 //       {props.children}
 //     </ul>
 //   );
@@ -35,7 +10,7 @@ export default Grid;
 
 // function GridItem(props: React.ComponentProps<'li'>) {
 //   return (
-//     <li {...props} className={clsx('flex flex-col', props.className)}>
+//     <li {...props} className={clsx('aspect-square transition-opacity', props.className)}>
 //       {props.children}
 //     </li>
 //   );
@@ -44,3 +19,28 @@ export default Grid;
 // Grid.Item = GridItem;
 
 // export default Grid;
+
+import clsx from 'clsx';
+
+function Grid(props: React.ComponentProps<'ul'>) {
+  return (
+    <ul
+      {...props}
+      className={clsx('grid grid-cols-2 gap-4 md:grid-cols-2 lg:grid-cols-3', props.className)}
+    >
+      {props.children}
+    </ul>
+  );
+}
+
+function GridItem(props: React.ComponentProps<'li'>) {
+  return (
+    <li {...props} className={clsx('flex flex-col', props.className)}>
+      {props.children}
+    </li>
+  );
+}
+
+Grid.Item = GridItem;
+
+export default Grid;
--- components/grid/tile.tsx
+++ components/grid/tile.tsx
@@ -18,25 +18,29 @@ export function GridTileImage({
   };
 } & React.ComponentProps<typeof Image>) {
   return (
-    <div
-      className={clsx(
-        'group flex h-full w-full items-center justify-center overflow-hidden rounded-lg border bg-white hover:border-blue-600 dark:bg-black',
-        {
-          relative: label,
-          'border-2 border-blue-600': active,
-          'border-neutral-200 dark:border-neutral-800': !active
-        }
-      )}
-    >
-      {props.src ? (
-        // eslint-disable-next-line jsx-a11y/alt-text -- `alt` is inherited from `props`, which is being enforced with TypeScript
-        <Image
-          className={clsx('relative h-full w-full object-contain', {
-            'transition duration-300 ease-in-out group-hover:scale-105': isInteractive
-          })}
-          {...props}
-        />
-      ) : null}
+    <>
+      <div
+        className={clsx(
+          'flex  h-full flex-col items-center justify-center overflow-hidden rounded-lg border bg-white hover:border-blue-600 dark:bg-black',
+          {
+            relative: label,
+            'border-2 border-blue-600': active,
+            'border-neutral-200 dark:border-neutral-800': !active
+          }
+        )}
+      >
+        {props.src ? (
+          // eslint-disable-next-line jsx-a11y/alt-text -- `alt` is inherited from `props`, which is being enforced with TypeScript
+          <div className="relative aspect-[2/3] h-full w-full">
+            <Image
+              className={clsx('relative  h-full w-full object-contain', {
+                'transition duration-300 ease-in-out group-hover:scale-105': isInteractive
+              })}
+              {...props}
+            />
+          </div>
+        ) : null}
+      </div>
       {label ? (
         <Label
           title={label.title}
@@ -45,6 +49,6 @@ export function GridTileImage({
           position={label.position}
         />
       ) : null}
-    </div>
+    </>
   );
 }
