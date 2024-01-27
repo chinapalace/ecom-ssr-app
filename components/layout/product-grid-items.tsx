90deb7d1a9cb4fc414eb41449a1ba9bbccc09136
chinapalace <justinmclaren63@gmail.com>
2024-01-26 17:13:23 -0800

    fix(openProduct): uses id instead of gid

--- components/layout/product-grid-items.tsx
+++ components/layout/product-grid-items.tsx
@@ -4,13 +4,17 @@ import { Product } from 'lib/shopify/types';
 import Link from 'next/link';
 
 export default function ProductGridItems({ products }: { products: Product[] }) {
+  const getIdFromGid = (gid: string) => {
+    const arr = gid.split('/');
+    return arr[arr.length - 1];
+  };
   return (
     <>
       {products.map((product, i) => (
         <Grid.Item key={product.handle} className="animate-fadeIn">
           <Link
             className="relative inline-block h-auto w-full"
-            href={`/open-product?id=${product.id}`}
+            href={`/open-product?id=${getIdFromGid(product.id)}`}
           >
             <GridTileImage
               alt={product.title}
