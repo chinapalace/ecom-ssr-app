9bb664746bc60bb3769d384daee5de7341eadd38
chinapalace <justinmclaren63@gmail.com>
2024-01-31 16:58:37 -0800

    feature(nosto): adds NostoItem component

--- app/(root)/nosto/nosto-provider.tsx
+++ app/(root)/nosto/nosto-provider.tsx
@@ -7,6 +7,24 @@ async function getTapcartIntegrations({ appId }) {
   console.log(data);
 }
 
+export function NostoItem({ product, onClick }) {
+  return (
+    <div className="nosto-item" onClick={onClick}>
+      <a href={product.url}>
+        <div className="nosto-image-container">
+          <div className="nosto-image">
+            <img src={product.thumb_url} alt={product.name} />
+          </div>
+          <div className="nosto-product-details">
+            <div className="nosto-product-name">{product.name}</div>
+            <div className="nosto-product-price">{product.price_text}</div>
+          </div>
+        </div>
+      </a>
+    </div>
+  );
+}
+
 export default async function NostoProvider({ children, nostoAccountId }) {
   //   const searchParams = useSearchParams();
   //   const appId = searchParams.get('appId')!;
@@ -14,5 +32,9 @@ export default async function NostoProvider({ children, nostoAccountId }) {
   //   const data = await res.json();
   //   console.log(data);
 
-  return <NostoProviderWrapper account={nostoAccountId}>{children}</NostoProviderWrapper>;
+  return (
+    <NostoProviderWrapper recommendationComponent={<NostoItem />} account={nostoAccountId}>
+      {children}
+    </NostoProviderWrapper>
+  );
 }
